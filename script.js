2c964fb0e1ca16b63f7992518561767d38a9d991
Aries Sky <jerifyemail@gmail.com>
2022-06-03 13:18:53 -0700

    new stuff

--- script.js
+++
@@ -1,7 +0,0 @@
-const { browser } = require("jshint/src/vars");
-
-document.getElementById("button1").onclick = function(){
-    document.location.href = "./pg2.html";
-    console.log("button clicked");
-    alert("button clicked");
-};
