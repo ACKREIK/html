511333c55448f7da746fdba769561902878a40f8
Aries Sky <jerifyemail@gmail.com>
2022-06-03 12:34:07 -0700

    initial commit

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-document.getElementById("continue").addEventListener("click", function(){
+const { browser } = require("jshint/src/vars");
 
+document.getElementById("continue").addEventListener("click", function(){
+    browser.openTab("./pg2.html");
+    browser.closeTab("./index.html");
     alert("You have successfully registered!");
 });
