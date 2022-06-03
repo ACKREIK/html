59452086aab3edb6cb576ac9618258620f5f83b8
Aries Sky <jerifyemail@gmail.com>
2022-06-03 12:43:48 -0700

    buttonfixagain

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const { browser } = require("jshint/src/vars");
 
-document.getElementById("continue").addEventListener("click", function(){
+document.getElementById("button").onclick = function(){
     browser.openTab("./pg2.html");
     browser.closeTab("./index.html");
     alert("You have successfully registered!");
-});
+};
